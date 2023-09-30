213ce3c6e37788f00785f13c3cbe7ac5e4e9c72b
Mayur900 <79475301+Mayur1992g@users.noreply.github.com>
2023-09-30 18:22:04 +0530

    ssdsdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componets/login/login.component';
+import { EmployeeListComponent } from './componets/employee-list/employee-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CreatEmployeeComponent } from './componets/creat-employee/creat-employee.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmployeeListComponent,
+    CreatEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
